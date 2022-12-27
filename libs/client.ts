d365eba51709a9759289d563018106f4d85d616d
koyaishida <59112617+koyaishida@users.noreply.github.com>
2022-12-27 21:45:01 +0900

    Delete client.ts

--- libs/client.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'microcms-js-sdk';
-
-export const client = createClient({
-    serviceDomain: '352y1e66sp',  // service-domain は XXXX.microcms.io の XXXX 部分
-    apiKey: 'W4y0AWBKAuQ3vVkGjWaa1adDmobGCT6pB5PB',
-});

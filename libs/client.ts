af8969cd8545f9e9dd1605d35f9340b32d3252d3
石田航也 <koyaishida@ishidawatarunarinoMacBook-Pro.local>
2022-12-27 21:16:57 +0900

    initial commit

---
+++ libs/client.ts
@@ -0,0 +1,6 @@
+import { createClient } from 'microcms-js-sdk';
+
+export const client = createClient({
+    serviceDomain: '352y1e66sp',  // service-domain は XXXX.microcms.io の XXXX 部分
+    apiKey: 'W4y0AWBKAuQ3vVkGjWaa1adDmobGCT6pB5PB',
+});
---
+++ types/blog.ts
@@ -0,0 +1,14 @@
+export type Blog = {
+    id: string
+    createdAt: string
+    updatedAt: string
+    publishedAt: string
+    revisedAt: string
+    title: string
+    content: string
+    eyecatch: {
+        url: string
+        height: number
+        width: number
+    },
+}
